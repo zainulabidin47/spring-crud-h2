38a28245299465382465f7092fca60f8bb2eda69
Zain ul Abidin <zabidin@i2cinc.com>
2023-01-12 17:31:39 +0500

    initial commit springboot 3.0 project

---
+++ src/main/java/com/zdevs/web/springcrudh2/SpringCrudH2Application.java
@@ -0,0 +1,13 @@
+package com.zdevs.web.springcrudh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrudH2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/zdevs/web/springcrudh2/SpringCrudH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zdevs.web.springcrudh2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudH2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
